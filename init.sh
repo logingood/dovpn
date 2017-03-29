f22795eee12250262a5eb26d8d26eeb2896f30dc
Murat Mukhtarov <muhtarov.mr@gmail.com>
2017-03-29 22:55:51 +1100

    Adjust strongswan config

--- init.sh
+++ init.sh
@@ -10,22 +10,35 @@ zfs create -o mountpoint=/var/lib/docker zroot/docker
 apt-get install -y docker-engine
 apt-get install -y letsencrypt strongswan
 apt-get install vim
-echo 192.168.99.2 : $1 >> /etc/ipsec.secrets
+apt-get install -y apparmor-utils
+aa-disable /usr/lib/ipsec/charon
+
+
+echo droplet phone : PSK \"${1}\" >> /etc/ipsec.secrets
 echo "syntax on" >> /root/.vimrc
 cat <<EOF > /etc/strongswan.conf
 # /etc/strongswan.conf - strongSwan configuration file
 
 charon {
-  load = random nonce aes sha1 sha2 curve25519 hmac stroke kernel-netlink socket-default updown
+	load_modular = yes
+
+	dns1 = 8.8.8.8
+	dns2 = 8.8.4.4
+
+	plugins {
+		include strongswan.d/charon/*.conf
+	}
 }
+
+include strongswan.d/*.conf
 EOF
 
 IP_ADDRESS=$(curl http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
 
 cat <<EOF > /etc/ipsec.conf
 # /etc/ipsec.conf - strongSwan IPsec configuration file
-
 config setup
+#  charondebug = "ike 2, cfg 1, enc 2"
 
 conn %default
 	ikelifetime=60m
@@ -37,8 +50,16 @@ conn %default
 
 conn rw
 	left=$IP_ADDRESS
-	leftsubnet=172.16.0.0/16
+	leftsubnet=0.0.0.0/0
 	leftfirewall=yes
+	keyexchange=ikev2
 	right=%any
+	rightsourceip=10.0.0.0/24
+	dpdaction=clear
+	leftid=droplet
+	rightid=phone
 	auto=add
+ 	ike=aes256-sha256-modp1024,aes128-sha1-modp1024,3des-sha1-modp1024! # Win7 is aes256, sha-1, modp1024; iOS is aes256, sha-256, modp1024; OS X is 3DES, sha-1, modp1024
+	esp=aes256-sha256,aes256-sha1,3des-sha1! # Win 7 is aes256-sha1, iOS is aes256-sha256, OS X is 3des-shal1
+	rekey=no
 EOF
