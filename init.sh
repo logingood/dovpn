111c347bdc5f7af14a19c7dd40c52b9ee65c2879
Murat Mukhtarov <muhtarov.mr@gmail.com>
2017-04-02 11:56:53 +1000

    Add set-e to the script

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -e
+
 apt-get update
 apt-get -y install zfsutils-linux
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
